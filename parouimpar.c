6fa4b495f06d84ececd3aaa92893c4eb0a694f69
Pedro Zeferino Bittencourt <pedro.zeferinob@gmail.com>
2025-05-18 20:37:00 -0300

    Delete parouimpar.c

--- parouimpar.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(void){
-    printf("digite um numero e responderei com 0 caso seja par e 1 caso seja impar");
-    int n;
-    scanf("%d", &n);
-    printf("%d", n%2);
-    
-}
