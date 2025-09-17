71f0d50c918e25d2856767884bdba71f5e2588eb
Pedro Zeferino Bittencourt <pedro.zeferinob@gmail.com>
2025-09-17 19:19:47 -0300

    Delete exercicios.c-1 directory

--- exercicios.c-1/aula1/ex003.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() { 
-
-int num = 359;
-int total = num/4;
-int resto = num%4;
-
-printf("distribuindo 359 carros para 4 pessoas, ficariam %d para cada e sobrariam %d na concessionaria", total, resto);
-}
